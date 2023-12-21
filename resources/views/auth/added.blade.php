266cdc2835329c4b309f40eea6011ff807f58738
tenpule <kogatana.tenpule@gmail.com>
2023-12-21 11:06:14 +0900

    田中さんを永田ですに変更。{{session('name')}}を追加。

--- resources/views/auth/added.blade.php
+++ resources/views/auth/added.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 
 <div id="clear">
-  <p>田中さん、</p>
+  <p>永田です</p>
   <p>ようこそ！DAWNSNSへ！</p>
   <p>ユーザー登録が完了しました。</p>
   <p>さっそく、ログインをしてみましょう。</p>
   <p class="btn"><a href="/login">ログイン画面へ</a></p>
 </div>
-
+{{session('name')}}
 @endsection
